479a387a20f3ab9d1ff35614fea8750753cf56d0
Your Name <rimonron.ad@gmail.com>
2024-02-01 01:17:23 +0600

    responsive sidebar

--- src/components/layouts/SideBar.tsx
+++ src/components/layouts/SideBar.tsx
@@ -1,12 +1,37 @@
+import { cn } from "@/lib/utils";
+import { LayoutDashboard } from "lucide-react";
 import { NavLink } from "react-router-dom";
 
 const SideBar = () =>{
     return (
-        <aside className="bg-green-200 col-span-2 h-screen sticky top-0 left-0 overflow-auto p-5">
-            <nav className="flex flex-col gap-2">
-                <NavLink  to="/admin" className="p-3 bg-green-300 rounded-md hover:text-white hover:bg-black transition-all">Dashboard</NavLink>
-                <NavLink  to="/admin/add-service" className="p-3 bg-green-300 rounded-md hover:text-white hover:bg-black">Add Service</NavLink>
-                <NavLink  to="/admin/service-list" className="p-3 bg-green-300 rounded-md hover:text-white hover:bg-black">Service List</NavLink>
+        <aside className="bg-green-200 col-span-2 h-screen sticky top-0 left-0 overflow-auto p-4 lg:p-5">
+            <nav className="flex flex-col gap-2 ">
+                <NavLink  to="/admin/dashboard" className={({isActive}) => cn("p-3 bg-green-300 rounded-md transition-all flex gap-2 items-center",
+                    {
+                        "text-white bg-black" : isActive
+                    }
+                    ) }>
+                        <LayoutDashboard className="shrink-0"></LayoutDashboard>
+                        <span className="truncate">Dashboard</span>
+                </NavLink>
+
+                <NavLink  to="/admin/add-service" className={({isActive}) => cn("p-3 bg-green-300 rounded-md transition-all flex gap-2 items-center",
+                    {
+                        "text-white bg-black" : isActive
+                    }
+                    ) }>
+                        <LayoutDashboard className="shrink-0"></LayoutDashboard>
+                        <span className="truncate">Add Service</span>
+                </NavLink>
+
+                <NavLink  to="/admin/service-list" className={({isActive}) => cn("p-3 bg-green-300 rounded-md transition-all flex gap-2 items-center",
+                    {
+                        "text-white bg-black" : isActive
+                    }
+                    ) }>
+                        <LayoutDashboard className="shrink-0"></LayoutDashboard>
+                        <span className="truncate">Service List</span>
+                </NavLink>
             </nav>
         </aside>
     )
