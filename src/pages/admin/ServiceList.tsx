6d4fe676f4318fdd53e5f3e923302ae19c6a0f25
Your Name <rimonron.ad@gmail.com>
2024-02-01 14:27:31 +0600

    TanStack query DevTools

--- src/pages/admin/ServiceList.tsx
+++ src/pages/admin/ServiceList.tsx
@@ -2,15 +2,6 @@ import { getServices } from "@/api/admin/service/service.api";
 import { useQuery } from "@tanstack/react-query";
 
 const ServiceList = () =>{
-    // const [data, setData] = useState([])
-
-    // console.log(data);
-
-    // useEffect(() =>{
-    //     fetch("http://localhost:5000/api/v1/services")
-    //     .then(res => res.json())
-    //     .then(data => setData(data.data))
-    // },)
 
     const {data, isLoading, isError} = useQuery({
         queryKey: ["services"],
