33fe1f6ae09ad0f65cd7b6f0b5991f9923c0f8d5
Your Name <rimonron.ad@gmail.com>
2024-02-01 23:15:50 +0600

    TanStack query data transformation with select

--- src/api/admin/service/service.api.ts
+++ src/api/admin/service/service.api.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 export const getServices = async() =>{
-    return await axios.get("http://localhost:5000/api/v1/servicesaa")
+    return await axios.get("http://localhost:5000/api/v1/services")
 }
 
 
--- src/pages/admin/ServiceList.tsx
+++ src/pages/admin/ServiceList.tsx
@@ -1,26 +1,66 @@
 import { getServices } from "@/api/admin/service/service.api";
+import Container from "@/components/Container";
+import { Button } from "@/components/ui/button";
+import { Table, TableBody, TableCell, TableFooter, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import { useQuery } from "@tanstack/react-query";
+import { Trash2 } from "lucide-react";
 
 const ServiceList = () =>{
 
-    const {data, isLoading, isError} = useQuery({
+    const {data: services, isLoading, isError} = useQuery({
         queryKey: ["services"],
-        queryFn: getServices
-    })
+        queryFn: getServices,
+        select: (data) =>{
+            const services = data?.data?.data.map((item) => ({
+                id: item._id,
+                name: item.name,
+                description: item.description,
+                price: item.price,
+            }))
 
-    console.log({isLoading, data, isError});
+            return services;
+        }
+    })
 
     if(isLoading){
         return <p>Loading .....</p>
     }
 
+    if(isError){
+        return <p>something went wrong .....</p>
+    }
 
     return (
-        <div>
-            {
-                data?.data?.map((item) => <h1>{item?.name}</h1>)
-            }
-        </div>
+        <Container className="mt-16 border p-0 rounded-2xl">
+        <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead>Name</TableHead>
+                <TableHead>Description</TableHead>
+                <TableHead>Price</TableHead>
+                <TableHead className="text-right">Action</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {services.map((service) => (
+                <TableRow key={service.id}>
+                  <TableCell className="font-medium">{service.name}</TableCell>
+                  <TableCell>{service.description}</TableCell>
+                  <TableCell>{service.price}</TableCell>
+                  <TableCell className="text-right">
+                    <Button className="p-2" variant="destructive"><Trash2></Trash2></Button>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+            <TableFooter>
+              <TableRow>
+                <TableCell colSpan={3}>Total Services</TableCell>
+                <TableCell className="text-right">{services.length}</TableCell>
+              </TableRow>
+            </TableFooter>
+        </Table>
+        </Container>
     )
 }
 
