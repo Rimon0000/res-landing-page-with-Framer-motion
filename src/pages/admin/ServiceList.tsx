4242b2b4b8b106655fd88aa56d0f61be5c2f0975
Your Name <rimonron.ad@gmail.com>
2024-02-01 23:26:35 +0600

    Custom data fetching hook

--- src/pages/admin/ServiceList.tsx
+++ src/pages/admin/ServiceList.tsx
@@ -1,26 +1,13 @@
 import { getServices } from "@/api/admin/service/service.api";
+import { useGetServices } from "@/api/admin/service/service.hook";
 import Container from "@/components/Container";
 import { Button } from "@/components/ui/button";
 import { Table, TableBody, TableCell, TableFooter, TableHead, TableHeader, TableRow } from "@/components/ui/table";
-import { useQuery } from "@tanstack/react-query";
 import { Trash2 } from "lucide-react";
 
 const ServiceList = () =>{
 
-    const {data: services, isLoading, isError} = useQuery({
-        queryKey: ["services"],
-        queryFn: getServices,
-        select: (data) =>{
-            const services = data?.data?.data.map((item) => ({
-                id: item._id,
-                name: item.name,
-                description: item.description,
-                price: item.price,
-            }))
-
-            return services;
-        }
-    })
+    const {data: services, isLoading, isError} = useGetServices();
 
     if(isLoading){
         return <p>Loading .....</p>
